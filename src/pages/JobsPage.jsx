7a98ccf5736969dc81050835985a79e594a03382
Ishita2803 <ishita2803@gmail.com>
2025-03-18 23:34:22 +0530

    react-job

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobListing from '../components/JobListing'
+
+const JobsPage = () => {
+  return (
+    <section className='bg-blue-90 px-5 py-6'>
+        <JobListing/>
+    </section>
+  )
+}
+
+export default JobsPage
